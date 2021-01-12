7fbf1a26a57d054a9c4d96671f6ce3f7aa0b8b4f
chriisyang <chris.yang@duke.edu>
2021-01-11 21:06:30 -0500

    added notification button details

--- src/NotificationList.js
+++ src/NotificationList.js
@@ -7,8 +7,12 @@ import Notification from './components/Notification';
 export default function NotificationList() {
   return (
     <>
-    <div>test if this will show up</div>
-    <Notification/>
+        <div>test if this will show up</div>
+        <Notification
+            isCompleteTask={true} 
+
+        />
+        <Notification/>
     </>
 
 
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,12 +1,31 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import stylesheet from './Notification.scss'
 import Plant from './Plant'; 
 
-export default function Notification() {
+export default function Notification( {isCompleteTask}) {
+
+  const taskButtonStyles = classNames({
+    'task-button': true,
+    'complete-task': isCompleteTask
+  })
+
   return (
+
     <div className='notification'> 
-      hello
+      <div className='task-details'>
+        <div className='task-date'>
+          Today, 12:00pm
+        </div>
+        <div className='task-name'> 
+          Water Pepperoni
+      </div>
     </div>
+      <div className={taskButtonStyles}>
+        Complete Task
+      </div>
+    </div>
+
 
   );
 }
